78963281b66235c95b4c5116552ce536f4947f64
SHAMUN AHMED <Shamyun Ahmed>
2023-11-27 20:22:25 +0600

    Started on the browser site.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import webbrowser
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def open_in_browser(url):
+    # todo: make it search up the site url from name and use it
+    webbrowser.open(url)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    url = input('say: ')
+    open_in_browser(f"www.{url}.com")
+    print('hello World!!!')
